b0a19893dd2a6f726ad1c55d484ae1f9c838378e
ishamashi <icho.ishamashi@gmail.com>
2023-05-15 18:51:26 +0700

    METADATA web

--- app/controllers/viewController.js
+++ app/controllers/viewController.js
@@ -2,19 +2,19 @@
 
 class ViewController {
   async index(req, res, next) {
-    res.render("layout", { body: "./page/index", title: "Techne.ID", scripts: ["materi.js"] });
+    res.render("layout", { body: "./page/index", title: "Techne Indonesia", scripts: ["materi.js"] });
   }
   async belajar(req, res, next) {
-    res.render("layout", { body: "./page/belajar", title: "express", scripts: ["materi.js"] });
+    res.render("layout", { body: "./page/belajar", title: "Techne Indonesia", scripts: ["materi.js"] });
   }
   async modul(req, res, next) {
-    res.render("layout", { body: "./page/materi", title: "express", scripts: ["modul.js"] });
+    res.render("layout", { body: "./page/materi", title: "Techne Indonesia", scripts: ["modul.js"] });
   }
   async materi(req, res, next) {
-    res.render("layout", { body: "./page/modul", title: "express", scripts: ["materi.js"] });
+    res.render("layout", { body: "./page/modul", title: "Techne Indonesia", scripts: ["materi.js"] });
   }
   async file(req, res, next) {
-    res.render("layout", { body: "./page/file", title: "express", scripts: ["file.js"] });
+    res.render("layout", { body: "./page/file", title: "Techne Indonesia", scripts: ["file.js"] });
   }
 }
 
