329570c968991a41262f6e1b99ff5d0c948a95bd
ishamashi <icho.ishamashi@gmail.com>
2023-12-10 21:15:14 +0700

    MK tryC

--- app/controllers/markdownController.js
+++ app/controllers/markdownController.js
@@ -18,21 +18,25 @@ md.use(require("markdown-it-table-of-contents"), {
 
 class MarkdownController {
   async getMarkdown(req, res, next) {
-    const parent = req?.params?.parent;
-    const fileSlug = req?.params?.fileName;
+    try {
+      const parent = req?.params?.parent;
+      const fileSlug = req?.params?.fileName;
 
-    let getData = await Materi.findFile(parent, fileSlug).catch((err) => err);
+      let getData = await Materi.findFile(parent, fileSlug).catch((err) => err);
 
-    const parentPath = getData.data[0].path;
-    const fileName = getData.data[0].name;
+      const parentPath = getData.data[0].path;
+      const fileName = getData.data[0].name;
 
-    const path = process.env.MATERI_PATH;
-    // String.raw`C:/Users/ichoi/OneDrive/Documents/ISHAMASHI/Project/materi`;
-    const testFolder = path + parentPath + fileName;
+      const path = process.env.MATERI_PATH;
+      // String.raw`C:/Users/ichoi/OneDrive/Documents/ISHAMASHI/Project/materi`;
+      const testFolder = path + parentPath + fileName;
 
-    const file = fs.readFileSync(testFolder, { encoding: "utf8", flag: "r" });
-    let data = md.render(file);
-    res.status(200).json({ parse: data, raw: file, data: getData.data[0] });
+      const file = fs.readFileSync(testFolder, { encoding: "utf8", flag: "r" });
+      let data = md.render(file);
+      res.status(200).json({ parse: data, raw: file, data: getData.data[0] });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
